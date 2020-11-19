09151380386432b5c9940d7511d6ba39c5e6053d
UrbanSaint <seseane9@gmail.com>
2020-11-20 01:20:33 +0200

    Login and Connection to BackendLess Complete

--- app/src/main/java/com/example/eventmanagementapp/MainActivity.java
+++ app/src/main/java/com/example/eventmanagementapp/MainActivity.java
@@ -1,13 +1,57 @@
 package com.example.eventmanagementapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Pair;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static int SPLASH_SCREEN=3000;
+    //Working Variables
+    Animation topAnimation,bottomAnimation;
+    ImageView imgLogo;
+    TextView tvSystem,tvTagline;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        //Animation
+        topAnimation= AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnimation= AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+
+        imgLogo=findViewById(R.id.imgLogo);
+        tvTagline=findViewById(R.id.tvTagline);
+        tvSystem=findViewById(R.id.tvSystem);
+
+        imgLogo.setAnimation(topAnimation);
+        tvSystem.setAnimation(bottomAnimation);
+        tvTagline.setAnimation(bottomAnimation);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(MainActivity.this,Login.class);
+                Pair[] pairs=new Pair[2];
+                pairs[0]=new Pair<View,String>(imgLogo,"logo_image");
+                pairs[1]=new Pair<View,String>(tvSystem,"logo_text");
+                ActivityOptions options=ActivityOptions.makeSceneTransitionAnimation(MainActivity.this,pairs);
+                startActivity(intent,options.toBundle());
+                finish();
+            }
+        },SPLASH_SCREEN);
+
+
     }
 }
